863e7ee76f191b90f9129d526df56506ad8df0bb
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-05-08 19:17:08 -0300

    Criando interface do repository no contexto de Pedidos

--- src/Orders/Buriti_Store.Orders.Domain/Interfaces/IOrderRepository.cs
+++ src/Orders/Buriti_Store.Orders.Domain/Interfaces/IOrderRepository.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Buriti_Store.Orders.Domain.Interfaces
 {
     public interface IOrderRepository
     {
+        Task<Order> GetById(Guid id);
+        Task<IEnumerable<Order>> GetListByClientId(Guid clientId);
+        Task<Order> GetOrderDraftByCustomerId(Guid clientId);
+        void Add(Order order);
+        void Update(Order order);
+
+        Task<OrderItem> GetItemById(Guid id);
+        Task<OrderItem> GetItemByOrder(Guid orderId, Guid productId);
+        void AddItem(OrderItem orderItem);
+        void UpdateItem(OrderItem orderItem);
+        void RemoveItem(OrderItem orderItem);
+
+        Task<Voucher> GetVoucherByCode(string code);
     }
 }

166d1dad0259f6849e4bd977422678c6c23473ec
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-05-08 18:42:44 -0300

    Criando contexto de Pedidos

---
+++ src/Orders/Buriti_Store.Orders.Domain/Enums/TypeVoucher.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buriti_Store.Orders.Domain.Enums
+{
+    public enum TypeVoucher
+    {
+
+    }
+}
---
+++ src/Orders/Buriti_Store.Orders.Domain/Voucher.cs
@@ -0,0 +1,14 @@
+﻿using Buriti_Store.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buriti_Store.Orders.Domain
+{
+    public class Voucher : Entity
+    {
+
+    }
+}

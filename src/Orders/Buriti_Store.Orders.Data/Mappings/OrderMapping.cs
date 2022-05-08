5ebcef8d8c93acbd0ed29d9dc9b5b6ef4e6c16f3
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-05-08 19:25:33 -0300

    Criando mappings e adicionando entity framework no contexto de Pedidos

---
+++ src/Orders/Buriti_Store.Orders.Data/Mappings/OrderMapping.cs
@@ -0,0 +1,24 @@
+﻿using Buriti_Store.Orders.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class OrderMapping : IEntityTypeConfiguration<Order>
+    {
+        public void Configure(EntityTypeBuilder<Order> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Code)
+                .HasDefaultValueSql("NEXT VALUE FOR MinhaSequencia");
+
+            // 1 : N => Pedido : PedidoItems
+            builder.HasMany(c => c.OrderItems)
+                .WithOne(c => c.Order)
+                .HasForeignKey(c => c.OrderId);
+
+            builder.ToTable("Orders");
+        }
+    }
+}
---
+++ src/Orders/Buriti_Store.Orders.Data/Mappings/VoucherMapping.cs
@@ -0,0 +1,26 @@
+﻿using Buriti_Store.Orders.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace NerdStore.Catalogo.Data.Mappings
+{
+    public class VoucherMapping : IEntityTypeConfiguration<Voucher>
+    {
+        public void Configure(EntityTypeBuilder<Voucher> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+
+            builder.Property(c => c.Code)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            // 1 : N => Voucher : Pedidos
+            builder.HasMany(c => c.Orders)
+                .WithOne(c => c.Voucher)
+                .HasForeignKey(c => c.VoucherId);
+
+            builder.ToTable("Vouchers");
+        }
+    }
+}

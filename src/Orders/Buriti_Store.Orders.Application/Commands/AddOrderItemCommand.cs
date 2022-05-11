b7bcaabbfadd95728c22e57a7163b8c1ad127902
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-05-10 22:47:45 -0300

    Criando primeiro command

---
+++ src/Orders/Buriti_Store.Orders.Application/Commands/AddOrderItemCommand.cs
@@ -0,0 +1,60 @@
+﻿using Buriti_Store.Core.Messages;
+using FluentValidation;
+using System;
+
+namespace Buriti_Store.Orders.Application.Commands
+{
+    public class AddOrderItemCommand : Command
+    {
+        public Guid ClientId { get; private set; }
+        public Guid ProductId { get; private set; }
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+        public decimal ValueUnity { get; private set; }
+
+        public AddOrderItemCommand(Guid clientId, Guid productId, string name, int quantity, decimal valueUnity)
+        {
+            ClientId = clientId;
+            ProductId = productId;
+            Name = name;
+            Quantity = quantity;
+            ValueUnity = valueUnity;
+        }
+
+        public override bool IsValid()
+        {
+            ValidationResult = new AddOrderItemValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class AddOrderItemValidation : AbstractValidator<AddOrderItemCommand>
+    {
+        public AddOrderItemValidation()
+        {
+            RuleFor(c => c.ClientId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do cliente inválido");
+
+            RuleFor(c => c.ProductId)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id do produto inválido");
+
+            RuleFor(c => c.Name)
+                .NotEmpty()
+                .WithMessage("O nome do produto não foi informado");
+
+            RuleFor(c => c.Quantity)
+                .GreaterThan(0)
+                .WithMessage("A quantidade miníma de um item é 1");
+
+            RuleFor(c => c.Quantity)
+                .LessThan(15)
+                .WithMessage("A quantidade máxima de um item é 15");
+
+            RuleFor(c => c.ValueUnity)
+                .GreaterThan(0)
+                .WithMessage("O valor do item precisa ser maior que 0");
+        }
+    }
+}

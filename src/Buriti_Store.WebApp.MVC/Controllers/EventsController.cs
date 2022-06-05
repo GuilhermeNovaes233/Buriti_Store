6bc83cd28e99462b6d947123aa0a8728900872fc
guilherme.novaes233 <guilherme.novaes233@gmail.com>
2022-06-05 16:29:06 -0300

    Criando controller  e views de eventos e pedidos

---
+++ src/Buriti_Store.WebApp.MVC/Controllers/EventsController.cs
@@ -0,0 +1,26 @@
+﻿using Buriti_Store.Core.Data.EventSourcing;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Buriti_Store.WebApp.MVC.Controllers
+{
+    public class EventsController : Controller
+    {
+        private readonly IEventSourcingRepository _eventSourcingRepository;
+
+        public EventsController(IEventSourcingRepository eventSourcingRepository)
+        {
+            _eventSourcingRepository = eventSourcingRepository;
+        }
+
+        [HttpGet("events/{id:guid}")]
+        public async Task<IActionResult> Index(Guid id)
+        {
+            var eventos = await _eventSourcingRepository.GetEvents(id);
+            return View(eventos);
+        }
+    }
+}
---
+++ src/Buriti_Store.WebApp.MVC/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Buriti_Store.Core.Communication.Mediator;
+using Buriti_Store.Core.Messages.CommonMessages.Notifications;
+using Buriti_Store.Orders.Application.Queries.Interfaces;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Buriti_Store.WebApp.MVC.Controllers
+{
+    public class OrderController : BaseController
+    {
+        private readonly IOrderQueries _orderQueries;
+
+        public OrderController(
+            IOrderQueries orderQueries,
+            INotificationHandler<DomainNotification> notifications,
+            IMediatorHandler mediatorHandler) : base(notifications, mediatorHandler)
+        {
+            _orderQueries = orderQueries;
+        }
+
+        [Route("my-orders")]
+        public async Task<IActionResult> Index()
+        {
+            return View(await _orderQueries.GetOrdersClient(ClientId));
+        }
+    }
+}
